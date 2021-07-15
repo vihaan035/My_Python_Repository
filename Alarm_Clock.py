922edc3b90c4ec4c274e21f65591c4babbf710a1
vihaan035 <vihaangupta035@gmail.com>
2021-07-15 17:47:15 +0530

    c

--- Alarm_Clock.py
+++ Alarm_Clock.py
@@ -3,7 +3,7 @@ from tkinter import *
 from win10toast import ToastNotifier
 import datetime
 import time
-#hello this is a test1
+#hello this is a test12
 def alarm(set_alarm):
     toast = ToastNotifier()
     while True:
